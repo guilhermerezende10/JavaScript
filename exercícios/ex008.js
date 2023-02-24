604d77a5adcc89838c5832a0572e15a9c632a11e
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-02-23 21:20:41 -0300

    javascript exercicios

---
+++ exercícios/ex008.js
@@ -0,0 +1,10 @@
+var vel = 2250.5
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+    
+}
+
+else {
+    console.log('Você não ultrapassou os limites de velocidades. Dirija sempre devagar!')
+}
---
+++ exercícios/ex009.js
@@ -0,0 +1,8 @@
+var country = 'Brasil'
+console.log(`Vivendo em ${country}`)
+if (country == 'Brasil') {
+    console.log('Tu é br né meu consagrado? eu sabia')
+}
+else {
+    console.log('Are you gringo brow?')
+}
