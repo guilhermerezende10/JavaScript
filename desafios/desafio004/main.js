4cf94ac91e5cca78a9d62bf126a1a532b15f08cc
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-03-11 18:50:03 -0300

    desafio 004

---
+++ desafios/desafio004/main.js
@@ -0,0 +1,18 @@
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {  
+        window.alert('Please, enter a number.')
+    } else {
+        let n = Number(num.value)
+        let c = 1 
+        tab.innerHTML = ''  
+        while (c <= 100 ) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
