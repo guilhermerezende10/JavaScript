d27f60bd3c41c79e1bec01c235f9e97fc27bf31a
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-03-16 20:22:19 -0300

    desafio005

--- desafios/desafio004/main.js
+++ desafios/desafio004/main.js
@@ -8,7 +8,7 @@ function tabuada() {
         let n = Number(num.value)
         let c = 1 
         tab.innerHTML = ''  
-        while (c <= 100 ) {
+        while (c <= 10 ) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             tab.appendChild(item)
---
+++ exercícios/function01.js
@@ -0,0 +1,11 @@
+function parimpar (n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'impar!'
+    }
+}
+
+
+console.log(parimpar(9))
+
