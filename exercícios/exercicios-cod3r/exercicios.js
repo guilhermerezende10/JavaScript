5016a272af897526a728feccd84f449282fa23c9
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-05-24 19:34:14 -0300

    ex 7

--- exercícios/exercicios-cod3r/exercicios.js
+++ exercícios/exercicios-cod3r/exercicios.js
@@ -50,3 +50,28 @@ const jurosCompostos = (CI, TJ, TA) => {
     return CI * (1+TJ)**TA
 }
 
+/*07) Uma das vantagens da programação é a automatização de tarefas que não gostamos de realizar. Dito isto,
+elabore uma função cujo objetivo é resolver a fórmula de Bhaskara. Para isso, sua função deve receber três
+parâmetros, “ax2”, “bx” e “c”, de tal modo que na equação: 3x² - 5x + 12 os valores seriam respectivamente: 3,
+-5, 12. Como retorno deve ser passado um vetor que tem 2 valores um para cada possível resultado, mesmo
+que os resultados sejam iguais. Caso o delta seja negativo, retorne, ao invés do vetor, um string com a frase:
+“Delta é negativo”. */
+
+const equacao2grau = (a, b, c) => {
+    let res = []
+    let delta = (b**2) - (4 * a * c)
+    if (delta < 0) {
+        return "Delta é negativo. Não existem raizes reais."
+    } 
+    let X1 = (-b + Math.sqrt(delta)) / (2 * a)
+    let X2 = (-b - Math.sqrt(delta)) / (2 * a)
+    res.push(X1)
+    res.push(X2)
+    let remover = res.toString().replace(/\[.*\]/g,'')
+    return remover
+}
+
+
+
+
+
