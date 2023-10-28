6a202baa35a700ed2c3b0dc33bfa3a6b8613103d
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-10-28 19:13:36 -0300

    Create stringMethodsPratice.js

---
+++ exercícios/curso-Jonas-Schmedtmann/challenges/Data Structures, Modern Operators and Strings/stringMethodsPratice.js
@@ -0,0 +1,10 @@
+const flights = 
+    '_Delayed_Departure;fao93766109;txl21333758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30'
+
+const getCode = str => str.slice(0,3).toUpperCase()
+
+for(const flight of flights.split('+')) {
+    const [type, from, to, time] = flight.split(';')
+    const output = `${type.startsWith('_Delayed') ? '🔴' : ''} ${type.replaceAll('_', ' ').trim()} from ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`.padStart(45)
+    console.log(output)
+}
