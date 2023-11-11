df84a7d3f3994dfb7db902f38d2ac6189b3db592
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-11-11 12:46:25 -0300

    Update bind.js

--- exercícios/curso-Jonas-Schmedtmann/exEmAula/bind.js
+++ exercícios/curso-Jonas-Schmedtmann/exEmAula/bind.js
@@ -37,3 +37,33 @@ const bookEW23 = book.bind(eurowings, 23)
 bookEW23('Maegor')
 bookEW23('Aegon')
 
+// With Event Listeners
+
+lufthansa.planes = 300
+lufthansa.buyPlane = function() {
+    console.log(this)
+    this.planes++
+    console.log(this.planes)
+}
+
+// document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa))
+
+const addTax = (rate, value) => value + value * rate
+
+console.log(addTax(0.2, 200))
+
+const addVAT = addTax.bind(null, 0.23)
+
+console.log(addVAT(1000))
+console.log(addVAT(284))
+
+const addTaxRate  = function(rate) {
+    return function(value) {
+        return value + value * rate
+    }
+}
+
+const addVAT2 = addTaxRate(0.23)
+
+console.log(addVAT2(100))
+console.log(addVAT2(23))
