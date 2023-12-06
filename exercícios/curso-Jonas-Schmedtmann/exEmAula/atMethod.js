4225ff0a79228eeeecfb9889c0df4f8d0ef89212
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-12-06 07:30:10 -0300

    Create atMethod.js

---
+++ exercícios/curso-Jonas-Schmedtmann/exEmAula/atMethod.js
@@ -0,0 +1,12 @@
+const arr = [23, 11, 64]
+console.log(arr[0])
+console.log(arr.at(0))
+
+// Getting last element
+
+console.log(arr[arr.length - 1])
+console.log(arr.slice(-1)[0])
+console.log(arr.at(-1)) // that is why it is useful
+
+console.log('jonas'.at(0))
+console.log('jonas'.at(-1))
