d0bdf33f5e059e99092c4bc9d880d65e05316848
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-12-13 10:09:55 -0300

    delete account implemented

--- exercícios/curso-Jonas-Schmedtmann/projects/Arrays-Bankist/script.js
+++ exercícios/curso-Jonas-Schmedtmann/projects/Arrays-Bankist/script.js
@@ -118,7 +118,7 @@ function updateUI(acc) {
     calcDisplaySummary(acc)
 }
 
-// event handler
+// event handlers
 let currentAccount; 
 
 btnLogin.addEventListener('click', function(e) {
@@ -151,6 +151,22 @@ btnTransfer.addEventListener('click', function(e) {
     updateUI(currentAccount)
 
   }
+})
+
+btnClose.addEventListener('click', function(e){
+  e.preventDefault()
+
+  if(inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin) {
+
+    const index = accounts.findIndex(acc => acc.username === currentAccount.username)
+
+    // Delete account
+    accounts.splice(index, 1)
+
+    // Hide UI
+    containerApp.style.opacity = 0
+  }
+  inputCloseUsername.value = inputClosePin.value = ''
 })
 
 
