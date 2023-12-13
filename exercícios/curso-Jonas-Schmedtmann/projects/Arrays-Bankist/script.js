23d5f0b6646037db7a2f725876ab57d4676dafbe
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-12-13 10:29:14 -0300

    request loan function

--- exercícios/curso-Jonas-Schmedtmann/projects/Arrays-Bankist/script.js
+++ exercícios/curso-Jonas-Schmedtmann/projects/Arrays-Bankist/script.js
@@ -153,6 +153,21 @@ btnTransfer.addEventListener('click', function(e) {
   }
 })
 
+btnLoan.addEventListener('click', function(e) {
+  e.preventDefault()
+  
+  const amount = Number(inputLoanAmount.value)
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.10)) {
+    // Add movement
+    currentAccount.movements.push(amount)
+
+    // Update UI
+    updateUI(currentAccount)
+  }
+  inputLoanAmount.value = ''
+})
+
 btnClose.addEventListener('click', function(e){
   e.preventDefault()
 
