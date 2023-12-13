232a0087dc4ddb04f0947e8ad407431abdeddab2
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-12-13 10:37:16 -0300

    Update someAndEvery.js

--- exercícios/curso-Jonas-Schmedtmann/exEmAula/someAndEvery.js
+++ exercícios/curso-Jonas-Schmedtmann/exEmAula/someAndEvery.js
@@ -7,7 +7,18 @@ console.log(movements)
 // EQUALITY
 console.log(movements.includes(200))
 
-// CONDITION
+// SOME: CONDITION
 console.log(movements.some(mov => mov === -130))
 const anyDeposits = movements.some(mov => mov > 1500)
 console.log(anyDeposits)
+
+// EVERY
+console.log(movements.every(mov => !isNaN(mov)))
+console.log(movements.every(mov => mov != 100))
+console.log(movements.every(mov => mov != 200))
+
+// Separate callback
+const deposit = mov => mov > 0
+console.log(movements.some(deposit))
+console.log(movements.every(deposit))
+console.log(movements.filter(deposit))
