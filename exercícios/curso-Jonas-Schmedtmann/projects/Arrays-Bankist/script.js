7f1201bcf8fcc1c03e7fed440f73e9df8e7eeb13
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-12-14 18:44:25 -0300

    sorting movements

--- exercícios/curso-Jonas-Schmedtmann/projects/Arrays-Bankist/script.js
+++ exercícios/curso-Jonas-Schmedtmann/projects/Arrays-Bankist/script.js
@@ -68,9 +68,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function(movements) {
+const displayMovements = function(movements, sort = false) {
     containerMovements.innerHTML = ''
-    movements.forEach(function(mov, i) {
+
+    const movs = sort ? movements.slice().sort((a, b) => a - b) : movements
+
+    movs.forEach(function(mov, i) {
     const type =  mov > 0 ? 'deposit' : 'withdrawal'
         const html = `
         <div class="movements__row">
@@ -191,6 +194,12 @@ btnClose.addEventListener('click', function(e){
   inputCloseUsername.value = inputClosePin.value = ''
 })
 
+let sorted = false
+btnSort.addEventListener('click', function(e) {
+  e.preventDefault()
+  displayMovements(currentAccount.movements, !sorted)
+  sorted = !sorted
+})
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
