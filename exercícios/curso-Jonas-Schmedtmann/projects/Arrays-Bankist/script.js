9fd26d2ad6eca55f7ebf549b1641cf5497af8af3
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-12-18 10:37:30 -0300

    implementing math functions

--- exercícios/curso-Jonas-Schmedtmann/projects/Arrays-Bankist/script.js
+++ exercícios/curso-Jonas-Schmedtmann/projects/Arrays-Bankist/script.js
@@ -144,7 +144,7 @@ const displayMovements = function (movements, sort = false) {
             <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-            <div class="movements__value">${mov}€</div>
+            <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>`;
 
     containerMovements.insertAdjacentHTML("afterbegin", html);
@@ -153,26 +153,26 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const inComes = acc.movements
     .filter((mov) => mov > 0)
     .reduce((acc, mov) => (acc += mov), 0);
-  labelSumIn.textContent = `${inComes}€`;
+  labelSumIn.textContent = `${inComes.toFixed(2)}€`;
 
   const outComes = acc.movements
     .filter((mov) => mov < 0)
     .reduce((acc, mov) => (acc += mov), 0);
-  labelSumOut.textContent = `${Math.abs(outComes)}€`;
+  labelSumOut.textContent = `${Math.abs(outComes).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter((mov) => mov > 0)
     .map((deposit) => (deposit * currentAccount.interestRate) / 100)
     .filter((deposit) => deposit >= 1)
     .reduce((acc, cur) => (acc += cur), 0);
-  labelSumInterest.textContent = `${interest.toFixed(1)}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -246,7 +246,7 @@ btnTransfer.addEventListener("click", function (e) {
 btnLoan.addEventListener("click", function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (
     amount > 0 &&
