b6bae0ae027dfbd10de1488446ae3181ddc6b862
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-12-18 18:22:26 -0300

    Create creatingDates.js

---
+++ exercícios/curso-Jonas-Schmedtmann/exEmAula/creatingDates.js
@@ -0,0 +1,9 @@
+
+
+// Create a date
+
+const now = new Date()
+console.log(now)
+console.log(new Date('2023-12-18T21:19:18.475Z'))
+console.log(new Date('December 25, 2023'))
+console.log(new Date(''))
