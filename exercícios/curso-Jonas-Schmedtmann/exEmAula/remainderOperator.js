b223ceea071f2f720aec87b8724cb424afea8a33
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-12-18 10:48:48 -0300

    Create remainderOperator.js

---
+++ exercícios/curso-Jonas-Schmedtmann/exEmAula/remainderOperator.js
@@ -0,0 +1,12 @@
+
+console.log(5 % 2) // remainder is 1
+console.log(5 / 2) // 5 = 2 * 2 + 1
+
+console.log(8 % 3)
+console.log(8 / 3) // 8 = 2 * 3 + 2
+
+const evenOrOdd = (n) => Math.floor(n % 2) ? 'odd' : 'even'
+console.log(evenOrOdd(4.3))
+console.log(evenOrOdd(13))
+console.log(evenOrOdd(2))
+console.log(evenOrOdd(3))
