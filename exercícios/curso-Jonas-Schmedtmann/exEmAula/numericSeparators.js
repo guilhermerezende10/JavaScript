377c436dbcf5e276447f63fe8b5bf20efc0f9bec
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-12-18 10:59:52 -0300

    Create numericSeparators.js

---
+++ exercícios/curso-Jonas-Schmedtmann/exEmAula/numericSeparators.js
@@ -0,0 +1,16 @@
+
+const diameter = 287_460_000_000; // 287,460,000,000
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee = 15_00
+const transferFee2 = 1_500
+
+const PI = 3.1415
+console.log(PI)
+
+console.log(Number('230000'))
+console.log(Number('230_000'))
+console.log(parseInt('230_000'))
