1be854c5a745dfc0520966437a25cc6cb26a46a8
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-12-19 08:53:42 -0300

    Update script.js

--- exercícios/curso-Jonas-Schmedtmann/projects/Arrays-Bankist/script.js
+++ exercícios/curso-Jonas-Schmedtmann/projects/Arrays-Bankist/script.js
@@ -356,16 +356,3 @@ btnSort.addEventListener("click", function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
-// const options = {
-//   style: 'currency',
-//   unit: 'celsius',
-//   currency: 'EUR',
-//   useGrouping: true
-// }
-
-// const num = 5782785434.49
-// console.log('US:', new Intl.NumberFormat('en-US', options).format(num))
-// console.log('BR:', new Intl.NumberFormat('pt-BR', options).format(num))
-// console.log('GER:', new Intl.NumberFormat('de-DE', options).format(num))
-// console.log('SYR:', new Intl.NumberFormat('ar-SY', options).format(num))
-// console.log('BROWSER:', new Intl.NumberFormat(navigator.language, options).format(num))
