2d71f3916428e6c4f5783341c43e61d475a12647
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-12-19 08:17:11 -0300

    Internationalizing Dates (Intl)

--- exercícios/curso-Jonas-Schmedtmann/projects/Arrays-Bankist/script.js
+++ exercícios/curso-Jonas-Schmedtmann/projects/Arrays-Bankist/script.js
@@ -132,7 +132,7 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-const formatMovementDate = function (date) {
+const formatMovementDate = function (date, locale) {
   const calcDaysPassed = (date1, date2) =>
     Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
   const daysPassed = calcDaysPassed(new Date(), date);
@@ -141,10 +141,7 @@ const formatMovementDate = function (date) {
   if (daysPassed === 1) return "Yesterday";
   if (daysPassed <= 7) return `${daysPassed} days ago`;
 
-  const day = `${date.getDate()}`.padStart(2, "0");
-  const month = `${date.getMonth() + 1}`.padStart(2, "0");
-  const year = date.getFullYear();
-  return `${day}/${month}/${year}`;
+  return new Intl.DateTimeFormat(locale).format(date)
 };
 
 const displayMovements = function (acc, sort = false) {
@@ -158,7 +155,7 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
     const date = new Date(acc.movementsDates[i]);
-    const displayDate = formatMovementDate(date);
+    const displayDate = formatMovementDate(date, acc.locale);
 
     const html = `
         <div class="movements__row">
@@ -226,8 +223,6 @@ currentAccount = account5;
 updateUI(currentAccount);
 containerApp.style.opacity = 100;
 
-// day/month/year
-
 btnLogin.addEventListener("click", function (e) {
   e.preventDefault(); // PREVENT FORM FROM SUBMITING
   currentAccount = accounts.find(
@@ -243,13 +238,23 @@ btnLogin.addEventListener("click", function (e) {
 
     containerApp.style.opacity = 100;
 
+    // Create current date and time
     const now = new Date();
-    const day = `${now.getDate()}`.padStart(2, "0");
-    const month = `${now.getMonth() + 1}`.padStart(2, "0");
-    const year = now.getFullYear();
-    const hour = `${now.getHours()}`.padStart(2, "0");
-    const min = `${now.getMinutes() + 1}`.padStart(2, "0");
-    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+    const options = {
+      hour: "numeric",
+      minute: "numeric",
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+      // weekday: 'long',
+    };
+
+    // const locale = navigator.language // picking the language of the navigator
+
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now);
 
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = "";
