f5c0253fd28614e7c684f1c83720fa89aab61595
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-12-23 09:39:15 -0300

    tabbed component

--- exercícios/curso-Jonas-Schmedtmann/projects/Bankist-Website/script.js
+++ exercícios/curso-Jonas-Schmedtmann/projects/Bankist-Website/script.js
@@ -59,6 +59,26 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+// Tabbed component
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+  
+  tabs.forEach(t => t.classList.remove('operations__tab--active'))
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'))
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+
+});
+
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
