1c2fe16788988ef7c251a71b423b8555fc713aab
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-12-23 10:39:20 -0300

    hover handler

--- exercícios/curso-Jonas-Schmedtmann/projects/Bankist-Website/script.js
+++ exercícios/curso-Jonas-Schmedtmann/projects/Bankist-Website/script.js
@@ -9,6 +9,10 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -61,22 +65,45 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 // Tabbed component
 
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
   if (!clicked) return;
-  
-  tabs.forEach(t => t.classList.remove('operations__tab--active'))
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
   clicked.classList.add('operations__tab--active');
 
   // Activate content area
 
-  tabsContent.forEach(t => t.classList.remove('operations__content--active'))
-  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'));
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
 
+// Menu fade animation
+
+const handleHover = function (e, opacity, scale, textDecoration) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = opacity;
+    });
+    link.style.scale = scale;
+    link.style.textDecoration = textDecoration;
+    logo.style.opacity = opacity;
+  }
+};
+
+nav.addEventListener('mouseover', function (e) {
+  //mouseover == mouseenter (but mouseenter does not booble)
+  handleHover(e, 0.5, 1.05, 'underline');
+});
+
+nav.addEventListener('mouseout', function (e) {
+  handleHover(e, 1, 1, 'none');
 });
 
 ////////////////////////////////////////////////////
