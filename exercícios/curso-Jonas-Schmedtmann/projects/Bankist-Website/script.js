48b2e6e24a100cd7a87a7d3e2633158039041981
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-12-23 10:48:32 -0300

    sticky navigation

--- exercícios/curso-Jonas-Schmedtmann/projects/Bankist-Website/script.js
+++ exercícios/curso-Jonas-Schmedtmann/projects/Bankist-Website/script.js
@@ -106,6 +106,16 @@ nav.addEventListener('mouseout', function (e) {
   handleHover(e, 1, 1, 'none');
 });
 
+// sticky navigation 
+const initialCoords = section1.getBoundingClientRect()
+console.log(initialCoords)
+
+window.addEventListener('scroll', function(e) {
+
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky')
+  else nav.classList.add('sticky')
+})
+
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
