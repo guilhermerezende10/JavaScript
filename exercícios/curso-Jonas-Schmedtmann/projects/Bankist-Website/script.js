0aef5fb5ac6e4af5e672ee10912d6af0f8fb1b2a
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-12-26 07:27:46 -0300

    lazy loading images

--- exercícios/curso-Jonas-Schmedtmann/projects/Bankist-Website/script.js
+++ exercícios/curso-Jonas-Schmedtmann/projects/Bankist-Website/script.js
@@ -137,10 +137,9 @@ const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
   if (!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden');
-  observer.unobserve(entry.target)
+  observer.unobserve(entry.target);
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
@@ -152,6 +151,28 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]'); // Selecting all images that has the attribute data-src
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if(!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src
+  entry.target.addEventListener('load', function(e) {
+    entry.target.classList.remove('lazy-img');
+  })
+  observer.unobserve(entry.target)
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px'
+});
+imgTargets.forEach(img => imgObserver.observe(img));
 
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
