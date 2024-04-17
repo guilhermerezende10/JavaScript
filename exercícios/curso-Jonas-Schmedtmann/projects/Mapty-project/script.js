54392689643c06192db82233a9ccd4c303ef9c47
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2024-04-17 18:01:00 -0300

    Displaying a Map Using Leaflet Library

--- exercícios/curso-Jonas-Schmedtmann/projects/Mapty-project/script.js
+++ exercícios/curso-Jonas-Schmedtmann/projects/Mapty-project/script.js
@@ -17,6 +17,18 @@ if(navigator.geolocation) {
         const {latitude} = position.coords
         const {longitude} = position.coords
         console.log(`https://www.google.com.br/maps/@${latitude},${longitude}`)
+
+        const coords = [latitude, longitude]
+
+        const map = L.map('map').setView(coords, 12);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+            .bindPopup('This is your aproximated location.')
+            .openPopup();
     }, function() {
         alert('Could not get your position, sorry. Try again later.')
     })
