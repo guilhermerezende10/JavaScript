7fc6451897b4bd8885141296489b7a111b42e36e
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2024-04-17 18:01:14 -0300

    Update script.js

--- exercícios/curso-Jonas-Schmedtmann/projects/Mapty-project/script.js
+++ exercícios/curso-Jonas-Schmedtmann/projects/Mapty-project/script.js
@@ -11,26 +11,29 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com.br/maps/@${latitude},${longitude}`);
 
-if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        const {latitude} = position.coords
-        const {longitude} = position.coords
-        console.log(`https://www.google.com.br/maps/@${latitude},${longitude}`)
+      const coords = [latitude, longitude];
 
-        const coords = [latitude, longitude]
+      const map = L.map('map').setView(coords, 12);
 
-        const map = L.map('map').setView(coords, 12);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-
-        L.marker(coords).addTo(map)
-            .bindPopup('This is your aproximated location.')
-            .openPopup();
-    }, function() {
-        alert('Could not get your position, sorry. Try again later.')
-    })
-    
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('This is your aproximated location.')
+        .openPopup();
+    },
+    function () {
+      alert('Could not get your position, sorry. Try again later.');
+    }
+  );
 }
