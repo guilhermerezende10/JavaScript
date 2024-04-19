017d1a1471e4a6d11e5004251fa9cf47add33267
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2024-04-19 06:31:34 -0300

    Displaying a Map Marker

--- exercícios/curso-Jonas-Schmedtmann/projects/Mapty-project/script.js
+++ exercícios/curso-Jonas-Schmedtmann/projects/Mapty-project/script.js
@@ -20,17 +20,28 @@ if (navigator.geolocation) {
 
       const coords = [latitude, longitude];
 
-      const map = L.map('map').setView(coords, 12);
+      const map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('This is your aproximated location.')
-        .openPopup();
+     
+        map.on('click', function(mapEvent) {
+          const {lat, lng} = mapEvent.latlng
+          L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+          }))
+          .setPopupContent('Workout')
+          .openPopup();
+        })
     },
     function () {
       alert('Could not get your position, sorry. Try again later.');
