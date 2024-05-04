aee7d9d87217cba008eb09f6c2ce5292adebca3d
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2024-05-04 10:58:14 -0300

    Working with localStorage

--- exercícios/curso-Jonas-Schmedtmann/projects/Mapty-project/script.js
+++ exercícios/curso-Jonas-Schmedtmann/projects/Mapty-project/script.js
@@ -74,8 +74,14 @@ class App {
   #mapEvent;
   #workouts = [];
   constructor() {
+
+    // Get user's position
     this._getPosition();
 
+    // Get data from local storage
+    this._getLocalStorage();
+
+    // Attach event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
 
     inputType.addEventListener('change', this._toggleElevationField);
@@ -110,6 +116,10 @@ class App {
 
     // Handling clicks on map
     this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach(work => {
+      this._renderWorkoutMarker(work)
+    })
   }
 
   _showForm(mapE) {
@@ -190,6 +200,10 @@ class App {
     // Hide form + Clear input fields
 
     this._hideForm();
+
+    // Set local storage to all workouts
+
+    this._setLocalStorage();
   }
 
   _renderWorkoutMarker(workout) {
@@ -264,12 +278,10 @@ class App {
   _moveToPopup(e) {
     const workoutEl = e.target.closest('.workout');
     if (!workoutEl) return;
-    console.log(workoutEl);
 
     const workout = this.#workouts.find(
       work => work.id === workoutEl.dataset.id
     );
-    console.log(workout);
 
     this.#map.setView(workout.coords, this.#mapZoomLevel, {
       animate: true,
@@ -280,7 +292,28 @@ class App {
 
     // using the public interface
 
-    workout.click();
+    // workout.click();
+  }
+
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts))
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'))
+
+    if(!data) return
+
+    this.#workouts = data
+
+    this.#workouts.forEach(work => {
+      this._renderWorkout(work)
+    })
+  }
+
+  reset() {
+    localStorage.removeItem('workouts')
+    location.reload()
   }
 }
 
