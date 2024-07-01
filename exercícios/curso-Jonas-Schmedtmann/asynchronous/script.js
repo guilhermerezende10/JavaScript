ca6e8b9a132a4d9cd42da6f0fa00482f500b0c72
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2024-07-01 13:00:21 -0300

    The Event Loop in Practice

--- exercícios/curso-Jonas-Schmedtmann/asynchronous/script.js
+++ exercícios/curso-Jonas-Schmedtmann/asynchronous/script.js
@@ -179,3 +179,17 @@ btn.addEventListener('click', function () {
 });
 
 // getCountryData('usa');
+
+
+console.log('Test start')
+setTimeout(() => console.log('0 timer sec'), 0)
+
+Promise.resolve('Resolved promise 1').then(res => console.log(res))
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for(let i = 0; i < 10000000000; i++) {}
+
+  console.log(res)
+})
+
+console.log('test end')
