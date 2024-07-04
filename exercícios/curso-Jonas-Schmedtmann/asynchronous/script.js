4435827a49d70e7c89ecc1bc331601e81a9d64e4
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2024-07-04 19:38:28 -0300

    Other Promise Combinators: race, allSettled and any

--- exercícios/curso-Jonas-Schmedtmann/asynchronous/script.js
+++ exercícios/curso-Jonas-Schmedtmann/asynchronous/script.js
@@ -354,22 +354,73 @@ const getJSON = function (url, errorMsg = 'Somethin went wrong') {
 
 // })();
 
-const get3Countries = async function (c1, c2, c3) {
-  try {
-    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
-    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
-    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+// const get3Countries = async function (c1, c2, c3) {
+//   try {
+//     // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+//     // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+//     // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
 
-    const data = await Promise.all([
-      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
-      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
-      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
-    ]);
+//     const data = await Promise.all([
+//       getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+//       getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+//       getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+//     ]);
 
-    console.log(data.map(d => d[0].capital[0]));
-  } catch (err) {
-    console.error(err);
-  }
+//     console.log(data.map(d => d[0].capital[0]));
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+
+// get3Countries('brazil', 'thailand', 'norway');
+
+// Promise.race
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v3.1/name/italy`),
+    getJSON(`https://restcountries.com/v3.1/name/france`),
+    getJSON(`https://restcountries.com/v3.1/name/singapore`),
+  ]);
+  // console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('Requeste took too long!'));
+    }, sec * 1000);
+  });
 };
 
-get3Countries('brazil', 'thailand', 'norway');
+Promise.race([
+  getJSON(`https://restcountries.com/v3.1/name/chile`),
+  timeout(0.64),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+// Promise.allSettled
+Promise.allSettled([ // Returns all promises, even the rejected ones
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another Success'),
+]).then(res => console.log(res));
+
+Promise.all([ // If there is a rejected promised, it will return a error
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another Success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+  // Promise.any [ES2021] // rejected promises will be ignored
+
+  Promise.any([
+    Promise.resolve('Success'),
+    Promise.reject('ERROR'),
+    Promise.resolve('Another Success'),
+  ])
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+
