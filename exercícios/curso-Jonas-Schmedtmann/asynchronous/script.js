79069775e2a12c3e31482d65722e21f6ef759f38
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2024-07-04 15:37:11 -0300

    Running Promises in Parallel

--- exercícios/curso-Jonas-Schmedtmann/asynchronous/script.js
+++ exercícios/curso-Jonas-Schmedtmann/asynchronous/script.js
@@ -135,13 +135,13 @@ const renderCountry = function (data, classname = '') {
 //     })
 // };
 
-// const getJSON = function (url, errorMsg = 'Somethin went wrong') {
-//   return fetch(url).then(response => {
-//     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+const getJSON = function (url, errorMsg = 'Somethin went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
 
-//     return response.json();
-//   });
-// };
+    return response.json();
+  });
+};
 
 // const getCountryData = function (country) {
 //   // Country 1
@@ -300,58 +300,76 @@ const renderCountry = function (data, classname = '') {
 
 // btn.addEventListener('click', whereAmI)
 
-const getPosition = function () {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-};
+// const getPosition = function () {
+//   return new Promise(function (resolve, reject) {
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
 
-const whereAmI = async function () {
+// const whereAmI = async function () {
+//   try {
+//     // Geolocation
+//     const pos = await getPosition();
+//     const { latitude: lat, longitude: lng } = pos.coords;
+
+//     // Reverse geocoding
+//     const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+//     if (!resGeo.ok) throw new Error('Problem with location data');
+
+//     const dataGeo = await resGeo.json();
+
+//     // Country Data
+//     // fetch(`https://restcountries.com/v3.1/name/${country}`).then(res => console.log(res)) // The same as below
+//     const res = await fetch(
+//       `https://restcountries.com/v3.1/name/${dataGeo.country}`
+//     );
+//     if (!res.ok) throw new Error('Problem getting country');
+//     // 'Await' will stop the execution until the promise is fulfilled
+
+//     const data = await res.json();
+//     renderCountry(data[0]);
+//     return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+//   } catch (err) {
+//     console.log(`${err} 🇪🇸`);
+//     renderError(`🚨${err.message}🚨`);
+
+//     // reject promised returned from async function
+//     throw err;
+//   }
+// };
+
+// console.log('1: Will get location');
+// // const city = whereAmI()
+// // console.log(city)
+
+// (async function () {
+//   try {
+//     const city = await whereAmI()
+//     console.log(`2: ${city}`)
+//   }
+//   catch(err) {
+//     console.error(`2: ${err.message}🔥`)
+//   }
+//   console.log('3: Finished getting location')
+
+// })();
+
+const get3Countries = async function (c1, c2, c3) {
   try {
-    // Geolocation
-    const pos = await getPosition();
-    const { latitude: lat, longitude: lng } = pos.coords;
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
 
-    // Reverse geocoding
-    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-    if (!resGeo.ok) throw new Error('Problem with location data');
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
 
-    const dataGeo = await resGeo.json();
-
-    // Country Data
-    // fetch(`https://restcountries.com/v3.1/name/${country}`).then(res => console.log(res)) // The same as below
-    const res = await fetch(
-      `https://restcountries.com/v3.1/name/${dataGeo.country}`
-    );
-    if (!res.ok) throw new Error('Problem getting country');
-    // 'Await' will stop the execution until the promise is fulfilled
-
-    const data = await res.json();
-    renderCountry(data[0]);
-    return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+    console.log(data.map(d => d[0].capital[0]));
   } catch (err) {
-    console.log(`${err} 🇪🇸`);
-    renderError(`🚨${err.message}🚨`);
-
-    // reject promised returned from async function
-    throw err;
+    console.error(err);
   }
 };
 
-console.log('1: Will get location');
-// const city = whereAmI()
-// console.log(city)
-
-(async function () {
-  try {
-    const city = await whereAmI()
-    console.log(`2: ${city}`)
-  }
-  catch(err) {
-    console.error(`2: ${err.message}🔥`)
-  }
-  console.log('3: Finished getting location')
-
-})();
-
-
+get3Countries('brazil', 'thailand', 'norway');
