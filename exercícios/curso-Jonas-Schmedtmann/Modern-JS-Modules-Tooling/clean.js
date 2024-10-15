913400f74414760652bf8c83ff9fdc7bae1951ab
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2024-10-15 11:01:24 -0300

    Let's Fix Some Bad Code: Part 2

--- exercícios/curso-Jonas-Schmedtmann/Modern-JS-Modules-Tooling/clean.js
+++ exercícios/curso-Jonas-Schmedtmann/Modern-JS-Modules-Tooling/clean.js
@@ -1,4 +1,6 @@
-const budget = [
+'strict mode';
+
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,44 +9,75 @@ const budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-const spendingLimits = {
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
+});
+
+const getLimit = (limits, user) => (limits[user] ? limits[user] : 0);
+
+// Pure function does not mutate anything outside itself
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'jonas'
+) {
+  const cleanUser = user.toLowerCase();
+
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value: -value, description, user: cleanUser }]
+    : state;
 };
 
-const getLimit = user => spendingLimits[user] ? spendingLimits[user] : 0;
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  100,
+  'Going to movies 🍿',
+  'Matilda'
+);
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
+console.log(newBudget3);
 
-const addExpense = function (value, description, user) {
-  if (!user) user = 'jonas';
-  user = user.toLowerCase();
+// const checkExpenses = function (state, limits) {
+//   return state.map(entry => {
+//     return entry.value < -getLimit(limits, entry.user) ? {...entry, flag: 'limit'} : entry
+//   })
 
-  if (value <= getLimit(user)) {
-    budget.push({ value: -value, description, user});
-  }
+//   // for (const entry of newBudget3)
+//   //   if (entry.value < -getLimit(limits, entry.user))
+//   //     entry.flag = 'limit';
+// };
+
+const checkExpenses = (state, limits) =>
+  state.map(entry =>
+    entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: 'limit' }
+      : entry
+  );
+
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+console.log(finalBudget);
+
+const logBigExpenses = function (state, bigLimit) {
+  return state
+    .filter(entry => entry.value <= -bigLimit)
+    .map(entry => `${entry.description.slice(-2)}`)
+    .join(' / ');
+
+  // let output = '';
+  // for (const entry of budget) {
+  //   output +=
+  //     entry.value <= -bigLimit ? `${entry.description.slice(-2)}/ ` : '';
+  // }
+  // output = output.slice(0, -2); // Remove last '/ '
+  // console.log(output);
 };
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
-console.log(budget);
 
-const checkExpenses = function () {
-  for (const entry of budget) 
-    if (entry.value < -getLimit(entry.user)) 
-      entry.flag = 'limit';
-};
-checkExpenses();
-
-console.log(budget);
-
-const logBigEnpenses = function (bigLimit) {
-  let output = '';
-  for (const entry of budget) {
-    output += entry.value <= -bigLimit ? `${entry.description.slice(-2)}/ ` : ''
-  }
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
-
-logBigEnpenses(1000)
+const bigExpenses = logBigExpenses(finalBudget, 1000);
+console.log(bigExpenses);
--- exercícios/curso-Jonas-Schmedtmann/asynchronous/script.js
+++ exercícios/curso-Jonas-Schmedtmann/asynchronous/script.js
@@ -342,35 +342,31 @@ const getJSON = function (url, errorMsg = 'Somethin went wrong') {
 // // const city = whereAmI()
 // // console.log(city)
 
-// (async function () {
-//   try {
-//     const city = await whereAmI()
-//     console.log(`2: ${city}`)
-//   }
-//   catch(err) {
-//     console.error(`2: ${err.message}🔥`)
-//   }
-//   console.log('3: Finished getting location')
+(async function () {
+  try {
+    const city = await whereAmI()
+    console.log(`2: ${city}`)
+  }
+  catch(err) {
+    console.error(`2: ${err.message}🔥`)
+  }
+  console.log('3: Finished getting location')
 
-// })();
+})();
 
-// const get3Countries = async function (c1, c2, c3) {
-//   try {
-//     // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
-//     // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
-//     // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
 
-//     const data = await Promise.all([
-//       getJSON(`https://restcountries.com/v3.1/name/${c1}`),
-//       getJSON(`https://restcountries.com/v3.1/name/${c2}`),
-//       getJSON(`https://restcountries.com/v3.1/name/${c3}`),
-//     ]);
-
-//     console.log(data.map(d => d[0].capital[0]));
-//   } catch (err) {
-//     console.error(err);
-//   }
-// };
+    console.log(data.map(d => d[0].capital[0]));
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 // get3Countries('brazil', 'thailand', 'norway');
 
