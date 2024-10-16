a6b7f2678c2d2cecebbe76b786405e7a8f1c0150
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2024-10-16 11:05:58 -0300

    Loading a Recipe from API

--- exercícios/curso-Jonas-Schmedtmann/Forkify App/src/js/controller.js
+++ exercícios/curso-Jonas-Schmedtmann/Forkify App/src/js/controller.js
@@ -11,3 +11,30 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza')
+    const data = await res.json()
+
+    if(!res.ok) throw new Error(`${data.message}`)
+
+    console.log(res, data)
+
+    let {recipe} = data.data
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    }
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe()
